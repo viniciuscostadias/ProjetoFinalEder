1f1829bbbe94ac0c30f3cab6f0d5b6f8cdf1b309
Vinícius da Costa Dias <131194529+viniciuscostadias@users.noreply.github.com>
2024-10-21 21:32:33 -0300

    encerrando o trabalho

---
+++ src/br/com/DAO/ConexaoDAO.java
@@ -0,0 +1,32 @@
+package br.com.DAO;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import javax.swing.JOptionPane;
+
+public class ConexaoDAO {
+
+    public static Connection connector() {
+
+        java.sql.Connection conexao = null;
+
+        String driver = "com.mysql.jdbc.Driver";
+
+        String url = "jdbc:mysql://localhost:3306/bdprojetoaula";
+
+        String user = "root";
+
+        String password = "080108";
+
+        try {
+            Class.forName(driver);
+
+            conexao = DriverManager.getConnection(url, user, password);
+            return conexao;
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "conexão falhou: " + e.getMessage());
+            return null;
+        }
+
+    }
+}
